b618b600b04e0aed33c2f421a1ee191e684310c3
Orchizakia00 <zakiaislam290@gmail.com>
2023-10-25 12:22:43 +0600

    login and register with email and password completed

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -13,10 +13,10 @@ const Services = () => {
 
     return (
         <div>
-            <div className="text-center my-6">
+            <div className="text-center mt-6">
                 <h2 className="text-xl text-orange-500 font-bold mb-3">Services</h2>
                 <h1 className="text-5xl font-bold mb-3">Our Service Area</h1>
-                <p className="w-1/2 mx-auto">
+                <p className="w-1/2 mx-auto mb-10">
                     The majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable.
                 </p>
             </div>
@@ -24,6 +24,9 @@ const Services = () => {
                 {
                     services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
                 }
+            </div>
+            <div className="my-8 text-center">
+                <button className="btn btn-outline border-orange-600 normal-case text-orange-600 hover:bg-orange-600 hover:text-white hover:border-none">More Services</button>
             </div>
         </div>
     );
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -3,20 +3,22 @@ import img from '../../assets/images/login/login.svg'
 import { useContext } from 'react';
 import { AuthContext } from '../../Providers/AuthProviders';
 
-const Login = () => {
+const Register = () => {
 
-    const {login} = useContext(AuthContext);
+    const {createUser} = useContext(AuthContext);
 
-    const handleLogin = event => {
+    const handleRegister = event => {
         event.preventDefault();
 
         const form = event.target;
+
+        const name = form.name.value;
         const email = form.email.value;
         const password = form.password.value;
 
-        console.log(email, password);
+        console.log(name, email, password);
 
-        login (email, password)
+        createUser(email, password)
         .then(result => {
             const user = result.user;
             console.log(user);
@@ -33,8 +35,14 @@ const Login = () => {
                     <img src={img} alt="" />
                 </div>
                 <div className="card flex-shrink-0 w-1/2 max-w-sm p-6 border">
-                    <form onSubmit={handleLogin} className="card-body">
-                        <h1 className="text-5xl font-bold text-center">Login</h1>
+                    <form onSubmit={handleRegister} className="card-body">
+                        <h1 className="text-5xl font-bold text-center">Register</h1>
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input name='name' type="text" placeholder="name" className="input input-bordered" required />
+                        </div>
                         <div className="form-control w-full">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -51,14 +59,14 @@ const Login = () => {
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <input className="btn bg-orange-600 normal-case text-white" type="submit" value="Login" />
+                            <input className="btn bg-orange-600 normal-case text-white" type="submit" value="Register" />
                         </div>
                     </form>
-                    <p className='text-center my-4'>Do not have an account? <Link className='text-orange-600 font-bold' to={'/register'}>Register</Link> </p>
+                    <p className='text-center my-4'>Already have an account? <Link className='text-orange-600 font-bold' to={'/login'}>Login</Link> </p>
                 </div>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default Register;
